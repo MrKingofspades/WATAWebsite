077d4873aa1907e408f467caedfcbb06b2a3c526
unknown <ericvining_70@yahoo.com>
2023-07-28 15:24:53 -0700

    Beginning code for WATA Website

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const App = () => {
+  return (
+    <div className='bg-black'>
+      <h1 className="text-white">Hello, World!</h1>
+    </div>
+  )
+}
+
+export default App
