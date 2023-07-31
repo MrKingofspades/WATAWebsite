78ec6935586214ec256576cee7b73a71141583c1
unknown <ericvining_70@yahoo.com>
2023-07-30 17:22:12 -0700

    Updated

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -15,12 +15,12 @@ const Navbar = () => {
     <body>
       <header className='w-full h-12 flex justify-between items-center z-50 fixed bg-nav-bg/60 shadow-xl shadow-neon-orange/20'>
         <div className='flex mx-4 sm:mx-8 mb-1'>
-          <h1 className='lg:hidden flex text-3xl text-neon-red font-lacquer'>WATA</h1>
-          <h1 className='hidden md:flex text-3xl text-neon-red font-lacquer'>WhoAreTheyActually</h1>
+          <h1 className='xl:hidden flex text-3xl text-neon-red font-lacquer'>WATA</h1>
+          <h1 className='hidden xl:flex text-3xl text-neon-red font-lacquer'>WhoAreTheyActually</h1>
         </div>
-        <nav className=' hidden md:flex items-center justify-between ml-[32rem]'>
-          <div className='absolute left-[22rem] -top-2'>
-            <ul className='hidden md:flex justify-between space-x-1'>
+        <nav className='flex items-center justify-between ml-[32rem]'>
+          <div className='absolute invisible md:visible left-[35%] sm:left-[40%] lg:left-[8rem] xl:left-[22rem] -top-2'>
+            <ul className='flex justify-between space-x-1'>
               <li className='py-3 px-1'>
                 <a href="https://www.instagram.com/entangledcerebrum/" target='_' className='text-neon-blue text-3xl hover:text-[#a7fdfd]'>{insta}</a>
               </li>
@@ -35,28 +35,32 @@ const Navbar = () => {
               </li>
             </ul>
           </div>
-          <ul className='hidden md:flex'>
-            <li className='border-r px-4 list-none'>
-              <a href='#' className='text-white font-ysabeau text-md p-2 hover:text-neon-purple hover:bg-[#7d7974]/30'>Home</a>
-            </li>
-            <li className='border-r px-4 list-none'>
-              <a href='#' className='text-white font-ysabeau text-md p-2 hover:text-neon-purple hover:bg-[#7d7974]/30'>About Us</a>
-            </li>
-            <li className='border-r px-4 list-none'>
-              <a href='#' className='text-white font-ysabeau text-md p-2 hover:text-neon-purple hover:bg-[#7d7974]/30'>Projects</a>
-            </li>
-            <li className='border-r px-4 list-none'>
-              <a href='#' className='text-white font-ysabeau text-md p-2 hover:text-neon-purple hover:bg-[#7d7974]/30'>Shop</a>
-            </li>
-            <li className='px-4 list-none'>
-              <a href='#' className='text-white font-ysabeau text-md p-2 hover:text-neon-purple hover:bg-[#7d7974]/30'>Contact Us</a>
-            </li>
-          </ul>
+          <div className='absolute left-[40%] lg:left-[42%] xl:left-[58%]'>
+            <ul className='flex invisible lg:visible'>
+              <li className='border-r px-4 list-none'>
+                <a href='#' className='text-white font-ysabeau text-md p-2 hover:text-neon-purple hover:bg-[#7d7974]/30'>Home</a>
+              </li>
+              <li className='border-r px-4 list-none'>
+                <a href='#' className='text-white font-ysabeau text-md p-2 hover:text-neon-purple hover:bg-[#7d7974]/30'>About Us</a>
+              </li>
+              <li className='border-r px-4 list-none'>
+                <a href='#' className='text-white font-ysabeau text-md p-2 hover:text-neon-purple hover:bg-[#7d7974]/30'>Projects</a>
+              </li>
+              <li className='border-r px-4 list-none'>
+                <a href='#' className='text-white font-ysabeau text-md p-2 hover:text-neon-purple hover:bg-[#7d7974]/30'>Shop</a>
+              </li>
+              <li className='px-4 list-none'>
+                <a href='#' className='text-white font-ysabeau text-md p-2 hover:text-neon-purple hover:bg-[#7d7974]/30'>Contact Us</a>
+              </li>
+            </ul>
+          </div>
         </nav>
-        <button className='w-8 h-8 text-3xl cursor-pointer relative hover:opacity-80 focus:outline-none md:hidden'>
-          <div className="bg-white w-8 h-1 rounded absolute top-3 right-6 before:content-[''] before:bg-white before before:w-8 before:h-1 before:rounded before:absolute before:-translate-y-3 before:-translate-x-4 after:content-[''] after:bg-white after after:w-8 after:h-1 after:rounded after:absolute after:translate-y-3 after:-translate-x-4"></div>
-        </button>
-        <button className='hidden mx-5 md:flex w-9 h-9 bg-nav-bg hover:bg-neon-yellow hover:shadow-md hover:shadow-neon-green rounded-full justify-center items-center'>
+        <div className='absolute left-[95%]'>
+          <button className='w-8 h-8 text-3xl cursor-pointer relative hover:opacity-80 focus:outline-none lg:hidden'>
+            <div className="bg-white w-8 h-1 rounded absolute top-3 right-6 before:content-[''] before:bg-white before before:w-8 before:h-1 before:rounded before:absolute before:-translate-y-3 before:-translate-x-4 after:content-[''] after:bg-white after after:w-8 after:h-1 after:rounded after:absolute after:translate-y-3 after:-translate-x-4"></div>
+          </button>
+        </div>
+        <button className='flex invisible mx-4 lg:visible w-9 h-9 bg-nav-bg hover:bg-neon-yellow hover:shadow-md hover:shadow-neon-green rounded-full justify-center items-center'>
           <div className='relative py-2'>
             <div className='text-neon-purple text-xl font-bold'>
               {cart}
